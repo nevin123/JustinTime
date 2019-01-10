8a11de8596f683e17eff1b84f0b5a8375984073d
Nevin Wouters <nevin-wouters@hotmail.com>
2019-01-10 20:05:07 +0100

    STUFF

---
+++ JustinTime_Unity/Assets/Scripts/Powerup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : ScriptableObject
+{
+    [SerializeField] Sprite powerupIcon;
+}
+
+[System.Serializable]
+public class Upgrades {
+    
+}
